ba5ff8c2147fd51aac158bb2db8af5be3a2231e5
Pasha Petukhov <pasha@emx.com>
2019-11-14 09:44:40 -0800

    minor change in tolerance use

--- refactor_me.py
+++ refactor_me.py
@@ -4,14 +4,14 @@ import math
 from collections import namedtuple, defaultdict
 from operator import itemgetter
 
-EPSILON = 1e-7
+EPSILON = 1e-9
 Expense = namedtuple('Expense', 'expense_name amount')
 
 
 def sum_expenses(expenses, min_amount=0):
     aggregated_expenses = defaultdict(int)
     for expense in expenses:
-        if expense.amount > min_amount or math.isclose(expense.amount, min_amount, abs_tol=EPSILON):
+        if expense.amount > min_amount or math.isclose(expense.amount, min_amount, rel_tol=EPSILON):
             aggregated_expenses[expense.expense_name.lower()] += expense.amount
     return aggregated_expenses
 
